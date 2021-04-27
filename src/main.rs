77b33d551b157eba342521e3458b9b357b5e0119
Tobias <derrumpli+github@posteo.de>
2021-04-27 20:26:48 +0200

    threading now works

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::vec::Vec;
-use std::collections::VecDeque;
 use std::{thread, time};
 
 /// Represents a result row of the /proc/stat content
@@ -36,90 +36,27 @@ enum ReadingMode {
     CpuValue,
 }
 
+struct CpuUtilization {
+    pub cpu_name: String,
+    pub utilization: f32,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let proc_stat = "/proc/stat";
-    /*
-    let mut previous_stat = ProcStatRow {
-        cpu_name: "cpu".to_string(), // ugly, should find better way
-        softirq: 0,
-        irq: 0,
-        iowait: 0,
-        idle: 0,
-        system_proc_kernel_mode: 0,
-        nice_proc_user_mode: 0,
-        normal_proc_user_mode: 0,
-    };*/
+    
     let mut stats: VecDeque<ProcStatRow> = VecDeque::new(); // create with fixed size
     let mut iteration_count = 0;
-    loop {
-        let file = File::open(proc_stat)?;
-        let reader = BufReader::new(file);
 
-        let mut reading_mode;
+    let proc_stat_thread = thread::spawn(move || loop {
+        update_current_cpu_utilization(&mut stats, &iteration_count);
 
-        for line in reader.lines() {
-            let row = line?;
-            if row.starts_with("cpu") {
-                reading_mode = ReadingMode::CpuName;
-
-                let mut current_cpu_name: &str = "";
-                let mut values: [u32; 10] = [0; 10];
-                let mut field_counter = 0;
-
-                for z in row.split_whitespace() {
-                    match reading_mode {
-                        ReadingMode::CpuName => {
-                            current_cpu_name = z;
-                            reading_mode = ReadingMode::CpuValue;
-                        }
-
-                        ReadingMode::CpuValue => {
-                            let number: u32 = match z.trim().parse() {
-                                Err(_) => 0,
-                                Ok(n) => n,
-                            };
-
-                            values[field_counter] = number;
-                            field_counter += 1;
-                        }
-                    }
-                }
-
-                let current_stat = ProcStatRow {
-                    cpu_name: current_cpu_name.to_string(), // ugly, should find better way
-                    softirq: values[6],
-                    irq: values[5],
-                    iowait: values[4],
-                    idle: values[3],
-                    system_proc_kernel_mode: values[2],
-                    nice_proc_user_mode: values[1],
-                    normal_proc_user_mode: values[0],
-                };
-            
-                
-                if iteration_count > 0 {
-                    //println!("{}", current_stat.cpu_name);
-                    let previous_stat = match stats.pop_front() {
-                        Some(x) => x,
-                        None => {
-                            break;
-                        },
-                    };
-                    //println!("{}", previous_stat.cpu_name);
-                    println!(
-                        "{} Utilization {}%",
-                        current_cpu_name,
-                        calculate_cpu_utilization(&previous_stat, &current_stat)
-                    );
-                }
-                stats.push_back(current_stat);
-            }
-        }
         let dur = time::Duration::from_millis(100);
         thread::sleep(dur);
 
         iteration_count += 1;
-    }
+    });
+
+    let res = proc_stat_thread.join();
 
     Ok(())
 }
@@ -132,4 +69,90 @@ fn calculate_cpu_utilization(previous: &ProcStatRow, current: &ProcStatRow) -> f
     let idle_delta = (current.idle - previous.idle) as f32;
     let utilization: f32 = 100.0 * (1.0 - idle_delta / total_delta);
     utilization
+}
+
+fn update_current_cpu_utilization(
+    stats: &mut VecDeque<ProcStatRow>,
+    iteration_count: &u32,
+) -> Vec<CpuUtilization> {
+    let file_handle = File::open("/proc/stat");
+
+    let file = match file_handle {
+        Ok(x) => x,
+        Err(_) => panic!(),
+    };
+
+    let reader = BufReader::new(file);
+
+    let mut reading_mode;
+
+    let mut result = Vec::<CpuUtilization>::new();
+
+    for line in reader.lines() {
+        let row = match line {
+            Ok(x) => x,
+            Err(_) => break,
+        };
+
+        if row.starts_with("cpu") {
+            reading_mode = ReadingMode::CpuName;
+
+            let mut current_cpu_name: &str = "";
+            let mut values: [u32; 10] = [0; 10];
+            let mut field_counter = 0;
+
+            for z in row.split_whitespace() {
+                match reading_mode {
+                    ReadingMode::CpuName => {
+                        current_cpu_name = z;
+                        reading_mode = ReadingMode::CpuValue;
+                    }
+
+                    ReadingMode::CpuValue => {
+                        let number: u32 = match z.trim().parse() {
+                            Err(_) => 0,
+                            Ok(n) => n,
+                        };
+
+                        values[field_counter] = number;
+                        field_counter += 1;
+                    }
+                }
+            }
+
+            let current_stat = ProcStatRow {
+                cpu_name: current_cpu_name.to_string(), // ugly, should find better way
+                softirq: values[6],
+                irq: values[5],
+                iowait: values[4],
+                idle: values[3],
+                system_proc_kernel_mode: values[2],
+                nice_proc_user_mode: values[1],
+                normal_proc_user_mode: values[0],
+            };
+
+            if *iteration_count > 0 {
+                //println!("{}", current_stat.cpu_name);
+                let previous_stat = match stats.pop_front() {
+                    Some(x) => x,
+                    None => {
+                        break;
+                    }
+                };
+                //println!("{}", previous_stat.cpu_name);
+                println!(
+                    "{} Utilization {}%",
+                    current_cpu_name,
+                    calculate_cpu_utilization(&previous_stat, &current_stat)
+                );
+                let utilization = CpuUtilization {
+                    cpu_name: current_cpu_name.to_string(),
+                    utilization: calculate_cpu_utilization(&previous_stat, &current_stat),
+                };
+                result.push(utilization);
+            }
+            stats.push_back(current_stat);
+        }
+    }
+    result 
 }
