06be818b38417e6fdff5b1e4d92c5a9a40ecb9a1
Tobias <derrumpli+github@posteo.de>
2021-04-29 21:28:50 +0200

    now prints multiple cpu stats

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::io;
 use std::vec::Vec;
 use std::{thread, time};
-use termion::{raw::IntoRawMode, event::Key};
+use termion::{event::Key, raw::IntoRawMode};
 use tui::{
     backend::{Backend, TermionBackend},
     layout::{Constraint, Direction, Layout, Rect},
-    widgets::{Block, Borders, Gauge},
-    terminal::Frame,
-    Terminal,
     style::{Color, Modifier, Style},
+    terminal::Frame,
+    text::Span,
+    symbols,
+    widgets::{Axis, Block, Borders, Chart, Gauge, Dataset, GraphType},
+    Terminal,
 };
 
 mod util;
@@ -27,7 +29,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-   
+
     // Initialize input handler
     let input_handler = util::InputHandler::new();
 
@@ -36,21 +38,46 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let sleep_duration = time::Duration::from_millis(100);
 
+    let mut core_values = Vec::<Vec<f64>>::new();
+    let mut cpu_values = Vec::<f64>::new();
+
+    //let mut cpu_values = Vec::<f64>::new();
     terminal.clear()?;
+    
     loop {
         let mem_info = match mem_dc_thread.try_recv() {
             Ok(a) => a,
             Err(_) => Default::default(),
         };
-        
+
         // Recv data from the data collector thread
-        /*let cpu_stats = match cpu_dc_thread.try_recv() {
+        let cpu_stats = match cpu_dc_thread.try_recv() {
             Ok(a) => a,
             Err(_) => vec![],
-        };*/
+        };
+        // create cpu info
+        let mut counter = 0;
+        for b in cpu_stats {
+            if b.cpu_name == "cpu"{
+                if cpu_values.len() == 300{
+                   cpu_values.remove(0);
+                } 
+                cpu_values.push(b.utilization);
+            }else {
+                if core_values.len() > counter {
+                    if core_values[counter].len() == 300{
+                       core_values[counter].remove(0);
+                    } 
+                    core_values[counter].push(b.utilization);
+                } else {
+                    core_values.push(Vec::new());
+                    core_values[counter].push(b.utilization);
+                }
+                counter += 1
+            }
+        }
 
-
-        let _ = terminal.draw(|f| {
+        terminal.draw(|f| {
             let chunks = Layout::default()
                 .direction(Direction::Vertical)
                 .margin(1)
@@ -67,25 +94,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             f.render_widget(block1, chunks[1]);
             let block2 = Block::default().title("Block2").borders(Borders::ALL);
             f.render_widget(block2, chunks[2]);
-            
-            draw_meminfo(f, &chunks, &mem_info);
 
-        });
+            draw_meminfo(f, chunks[0], &mem_info);
+            draw_cpuinfo(f, chunks[1], &cpu_values, &core_values);
+        })?;
 
         // Handle events
         match input_handler.next() {
             Ok(InputEvent::Input(input)) => {
                 match input {
                     Key::Ctrl('c') => {
-                        terminal.clear();
+                        terminal.clear()?;
                         break;
-                    },
-                    _ => {},
+                    }
+                    _ => {}
                 };
-            },
-            Err(_)=> {},
+            }
+            Err(_) => {}
         }
-
 
         // Sleep
         thread::sleep(sleep_duration);
@@ -93,29 +119,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn draw_meminfo<B: Backend>(f: &mut Frame<B>, chunks: &Vec<Rect>, mem_info: &MemInfo) {
-        let boxes = Layout::default()
+fn draw_meminfo<B: Backend>(f: &mut Frame<B>, rect: Rect, mem_info: &MemInfo) {
+    let boxes = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints(
-            [
-                Constraint::Percentage(50),
-                Constraint::Percentage(50),
-            ]
-            .as_ref(),
-        )
-        .split(chunks[0]);
-        let block_chunks = Layout::default()
-            .constraints([Constraint::Length(2), Constraint::Length(2)])
-            .margin(1)
-            .split(boxes[0]);
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+        .split(rect);
+    let block_chunks = Layout::default()
+        .constraints([Constraint::Length(2), Constraint::Length(2)])
+        .margin(1)
+        .split(boxes[0]);
 
-        let block = Block::default().title("Mem").borders(Borders::ALL);
-        f.render_widget(block, boxes[0]);
-        
-        // calc mem infos
-        let mem_usage = (mem_info.mem_total - mem_info.mem_available) / mem_info.mem_total;
-        let mem_swap = mem_info.swap_cached / mem_info.swap_total;
-        let label_mem = format!("{:.2}%", mem_usage * 100.0);
+    let block = Block::default().title("Mem").borders(Borders::ALL);
+    f.render_widget(block, boxes[0]);
+
+    // calc mem infos
+    let mem_usage = (mem_info.mem_total - mem_info.mem_available) / mem_info.mem_total;
+    let mem_swap = mem_info.swap_cached / mem_info.swap_total;
+    let label_mem = format!("{:.2}%", mem_usage * 100.0);
+
+    if 0.0 <= mem_usage && mem_usage <= 1.0 {
         let gauge_mem = Gauge::default()
             .block(Block::default().title("Mem:"))
             .gauge_style(
@@ -127,7 +149,11 @@ fn draw_meminfo<B: Backend>(f: &mut Frame<B>, chunks: &Vec<Rect>, mem_info: &Mem
             .label(label_mem)
             .ratio(mem_usage);
         f.render_widget(gauge_mem, block_chunks[0]);
-        let label_swap = format!("{:.2}%", mem_swap * 100.0);
+    }
+
+    let label_swap = format!("{:.2}%", mem_swap * 100.0);
+
+    if 0.0 <= mem_swap && mem_swap <= 1.0 {
         let gauge_swap = Gauge::default()
             .block(Block::default().title("Swap:"))
             .gauge_style(
@@ -139,4 +165,58 @@ fn draw_meminfo<B: Backend>(f: &mut Frame<B>, chunks: &Vec<Rect>, mem_info: &Mem
             .label(label_swap)
             .ratio(mem_swap);
         f.render_widget(gauge_swap, block_chunks[1]);
+    }
+}
+
+
+fn draw_cpuinfo<B: Backend>(f: &mut Frame<B>, rect: Rect, data: &Vec<f64>, cores: &Vec<Vec<f64>>) {
+    let v = data.iter().enumerate().map(|(i, &x)| (i as f64, x)).collect::<Vec<_>>();
+
+    let mut datasets = vec![
+        Dataset::default()
+            .name("cpu")
+            .marker(symbols::Marker::Dot)
+            .style(Style::default().fg(Color::Cyan))
+            .graph_type(GraphType::Line)
+            .data(&v),
+    ];
+
+    let mut core_values = Vec::new();
+    for core in cores {
+        let value = core.iter().enumerate().map(|(i, &x)| (i as f64, x)).collect::<Vec<_>>();
+        core_values.push(value);
+    }
+
+    for i in 0..core_values.len() {
+        datasets.push(Dataset::default()
+            .name(format!("cpu{}", i))
+            .marker(symbols::Marker::Dot)
+            .style(Style::default().fg(Color::Indexed(i as u8)))
+            .graph_type(GraphType::Line)
+            .data(&core_values[i]));
+    }
+
+    let chart = Chart::new(datasets)
+        .block(
+            Block::default()
+                .title(Span::styled(
+                    "CPU",
+                    Style::default()
+                        .fg(Color::Cyan)
+                        .add_modifier(Modifier::BOLD),
+                ))
+                .borders(Borders::ALL),
+        )
+        .x_axis(Axis::default().bounds([0.0, 300.0]))
+        .y_axis(
+            Axis::default()
+                .style(Style::default().fg(Color::Gray))
+                .labels(vec![
+                    Span::styled("0", Style::default().add_modifier(Modifier::BOLD)),
+                    Span::styled("100", Style::default().add_modifier(Modifier::BOLD)),
+                ])
+                .bounds([0.0, 100.0]),
+        );
+
+    f.render_widget(chart, rect);
 }
