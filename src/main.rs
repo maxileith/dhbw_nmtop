f3c59480fa0139426864332971a5b165c8335c70
Tobias <derrumpli+github@posteo.de>
2021-05-02 21:23:45 +0200

    adds option to select a specific widget

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use std::{thread, time};
 use termion::{event::Key, raw::IntoRawMode};
 use tui::{
     backend::{Backend, TermionBackend},
-    layout::{Constraint, Direction, Layout, Rect},
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     symbols,
     terminal::Frame,
     text::{Span, Spans},
     widgets::{
-        Axis, Block, Borders, Cell, Chart, Dataset, Gauge, GraphType, Paragraph, Row, Table, Wrap,
+        Axis, Block, Borders, BorderType, Cell, Chart, Dataset, Gauge, GraphType, Paragraph, Row, Table, Wrap,
     },
     Terminal,
 };
@@ -36,12 +36,68 @@ use processes::ProcessList;
 mod network;
 use network::{to_humanreadable, NetworkInfo};
 
+
+#[derive(PartialEq)]
+enum AppState {
+    Navigation,
+    Interaction,
+}
+
+struct AppLogic {
+    state: AppState,
+    current_widget: WidgetType,
+}
+
+#[derive(PartialEq)]
+enum WidgetType {
+    CPU,
+    Network,
+    Disk,
+    Processes,
+    Memory,
+}
+
+impl WidgetType {
+    // returns id, name
+    fn get_value(&self) -> (usize, &str) {
+        match *self {
+            WidgetType::Memory=> (0, "Memory"),
+            WidgetType::Disk => (1, "Disk"),
+            WidgetType::Network => (2, "Network"),
+            WidgetType::CPU => (3, "CPU"),
+            WidgetType::Processes => (4, "Processes"),
+        }
+    }
+
+    fn get_by_id(id: usize) -> WidgetType {
+        match id {
+            0 => WidgetType::Memory,
+            1 => WidgetType::Disk,
+            2 => WidgetType::Network,
+            3 => WidgetType::CPU,
+            4 => WidgetType::Processes,
+            _ => WidgetType::Memory, //default case
+        }
+    }
+}
+
+struct DataWidget {
+    typ: WidgetType,
+}
+
 // TODO: user input to stop execution
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Terminal initialization
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+
+    // Initialize app state
+    let mut app = AppLogic {
+        state: AppState::Interaction,
+        current_widget: WidgetType::Memory,
+    };
+
     // Initialize input handler
     let input_handler = util::InputHandler::new();
 
@@ -51,13 +107,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let processes_dc_thread = processes::init_data_collection_thread();
     let network_dc_thread = network::init_data_collection_thread();
 
-    let sleep_duration = time::Duration::from_millis(500);
+    let sleep_duration = time::Duration::from_millis(100);
 
     let mut core_values = Vec::<Vec<f64>>::new();
     let mut cpu_values = Vec::<f64>::new();
     let mut last_network_info: NetworkInfo = Default::default();
 
     let mut processes_info: ProcessList = Default::default();
+
+    let data_widgets = vec![WidgetType::Memory, WidgetType::Disk, WidgetType::Network, WidgetType::CPU, WidgetType::Processes];
 
     //let mut cpu_values = Vec::<f64>::new();
     terminal.clear()?;
@@ -118,42 +176,102 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         Constraint::Length(6),
                         Constraint::Length(10),
                         Constraint::Min(1),
+                        Constraint::Length(1),
                     ]
                     .as_ref(),
                 )
                 .split(f.size());
             let boxes = Layout::default()
                 .direction(Direction::Horizontal)
-                .constraints([Constraint::Percentage(30), Constraint::Percentage(45), Constraint::Percentage(25)].as_ref())
+                .constraints(
+                    [
+                        Constraint::Percentage(30),
+                        Constraint::Percentage(45),
+                        Constraint::Percentage(25),
+                    ]
+                    .as_ref(),
+                )
                 .split(chunks[0]);
-            // let block1 = Block::default().title("Block 2").borders(Borders::ALL);
-            // f.render_widget(block1, chunks[1]);
-            // let block2 = Block::default().title("Block 3").borders(Borders::ALL);
-            // f.render_widget(block2, chunks[2]);
+           
+            // Draw data widgets
+            for dw in &data_widgets {
+                let (id, name)  = dw.get_value();
+                
+                let selected = id  == app.current_widget.get_value().0 && app.state == AppState::Navigation;
 
-            // print!("{:?}", processes_info.processes);
+                match dw {
+                    WidgetType::Memory => {
+                        draw_meminfo(f, boxes[0], create_block(name, selected), &mem_info);
+                    },
+                    WidgetType::Disk=> {
+                        draw_diskinfo(f, boxes[1], create_block(name, selected), &disk_info);
+                    },
+                    WidgetType::Network=> {
+                        draw_networkinfo(f, boxes[2], create_block(name, selected), &last_network_info, &network_info);
+                    },
+                    WidgetType::CPU=> {
+                        draw_cpuinfo(f, chunks[1], create_block(name, selected), &cpu_values, &core_values);
+                    },
+                    WidgetType::Processes=> {
+                        draw_processesinfo(f, chunks[2], create_block(name, selected),  &processes_info);
+                    },
+                }
+            }
 
-            draw_cpuinfo(f, chunks[1], &cpu_values, &core_values);
-            draw_meminfo(f, &boxes, &mem_info);
-            draw_diskinfo(f, &boxes, &disk_info);
-            draw_processesinfo(f, chunks[2], &processes_info);
-            draw_networkinfo(f, boxes[2], &last_network_info, &network_info);
+            // Draw help text
+            let help_paragraph = Paragraph::new("ESC: navigation").block(Block::default()).alignment(Alignment::Left);
+            f.render_widget(help_paragraph, chunks[3]);
+            
         });
 
         last_network_info = network_info;
 
         // Handle events
-        match input_handler.next() {
-            Ok(InputEvent::Input(input)) => {
-                match input {
-                    Key::Ctrl('c') => {
-                        terminal.clear()?;
-                        break;
+        let event = input_handler.next();
+        
+        if event.is_ok() {
+            let event = event.unwrap();
+            if let InputEvent::Input(input) = event{
+                match app.state {
+                    AppState::Interaction => {
+                        match input {
+                            Key::Ctrl('c') => {
+                                terminal.clear()?;
+                                break;
+                            }
+                            Key::Esc => {
+                                app.state = AppState::Navigation;
+                            }
+                            _ => {}
+                        };
                     }
-                    _ => {}
-                };
+
+                    AppState::Navigation => {
+                        match input {
+                            Key::Ctrl('c') => {
+                                terminal.clear()?;
+                                break;
+                            }
+                            Key::Right => {
+                                let (id, _) = app.current_widget.get_value();
+                                if id < data_widgets.len() - 1 {
+                                    app.current_widget = WidgetType::get_by_id(id + 1); 
+                                }
+                            }
+                            Key::Left => {
+                                let (id, _) = app.current_widget.get_value();
+                                if id > 0 {
+                                    app.current_widget = WidgetType::get_by_id(id - 1); 
+                                }
+                            }
+                            Key::Esc => {
+                                app.state = AppState::Interaction;
+                            }
+                            _ => {}
+                        };
+                    }
+                }
             }
-            Err(_) => {}
         }
 
         // Sleep
@@ -162,20 +280,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn draw_meminfo<B: Backend>(f: &mut Frame<B>, boxes: &Vec<Rect>, mem_info: &MemInfo) {
+fn create_block(name: &str, selected: bool) -> Block{
+    let block = Block::default()
+        .title(Span::styled(
+            name,
+            Style::default()
+                .fg(Color::Cyan)
+                .add_modifier(Modifier::BOLD),
+        ))
+        .borders(Borders::ALL);
+
+    if selected {
+        return block.border_type(BorderType::Thick);
+    }
+
+    block
+}
+
+fn draw_meminfo<B: Backend>(f: &mut Frame<B>, rect: Rect, block: Block, mem_info: &MemInfo) {
     let block_chunks = Layout::default()
         .constraints([Constraint::Length(2), Constraint::Length(2)])
         .margin(1)
-        .split(boxes[0]);
+        .split(rect);
 
-    let block = Block::default()
-        .title(Span::styled(
-        "Memory",
-        Style::default()
-            .fg(Color::Cyan)
-            .add_modifier(Modifier::BOLD),
-        )).borders(Borders::ALL);
-    f.render_widget(block, boxes[0]);
+    // Render block
+    f.render_widget(block, rect);
 
     // calc mem infos
     let mem_usage =
@@ -215,7 +344,7 @@ fn draw_meminfo<B: Backend>(f: &mut Frame<B>, boxes: &Vec<Rect>, mem_info: &MemI
     f.render_widget(gauge_swap, block_chunks[1]);
 }
 
-fn draw_cpuinfo<B: Backend>(f: &mut Frame<B>, rect: Rect, data: &Vec<f64>, cores: &Vec<Vec<f64>>) {
+fn draw_cpuinfo<B: Backend>(f: &mut Frame<B>, rect: Rect, block: Block, data: &Vec<f64>, cores: &Vec<Vec<f64>>) {
     let mut datasets = Vec::new();
 
     let mut core_values = Vec::new();
@@ -270,16 +399,7 @@ fn draw_cpuinfo<B: Backend>(f: &mut Frame<B>, rect: Rect, data: &Vec<f64>, cores
     );
 
     let chart = Chart::new(datasets)
-        .block(
-            Block::default()
-                .title(Span::styled(
-                    "CPU",
-                    Style::default()
-                        .fg(Color::Cyan)
-                        .add_modifier(Modifier::BOLD),
-                ))
-                .borders(Borders::ALL),
-        )
+        .block(block)
         .x_axis(Axis::default().bounds([0.0, 300.0]))
         .y_axis(
             Axis::default()
@@ -294,15 +414,8 @@ fn draw_cpuinfo<B: Backend>(f: &mut Frame<B>, rect: Rect, data: &Vec<f64>, cores
     f.render_widget(chart, rect);
 }
 
-fn draw_diskinfo<B: Backend>(f: &mut Frame<B>, boxes: &Vec<Rect>, disk_info: &Vec<DiskInfo>) {
+fn draw_diskinfo<B: Backend>(f: &mut Frame<B>, rect: Rect, block:Block, disk_info: &Vec<DiskInfo>) {
     //draw disk info TODO: divide into own function
-    let block = Block::default()
-        .title(Span::styled(
-        "Disks",
-        Style::default()
-            .fg(Color::Cyan)
-            .add_modifier(Modifier::BOLD),
-        )).borders(Borders::ALL);
     let header_cells = ["Partition", "Available", "In Use", "Total", "Used", "Mount"]
         .iter()
         .map(|h| Cell::from(*h).style(Style::default().fg(Color::White)));
@@ -317,27 +430,25 @@ fn draw_diskinfo<B: Backend>(f: &mut Frame<B>, boxes: &Vec<Rect>, disk_info: &Ve
         cells.push(Cell::from(disk.mountpoint.clone()));
         Row::new(cells)
     });
-    let sizing = &size_columns(boxes[1].width);
+    let sizing = &size_columns(rect.width);
     let table = Table::new(rows)
         .header(header)
         .block(block)
         .widths(sizing)
         .column_spacing(2);
 
-    f.render_widget(table, boxes[1]);
+    f.render_widget(table, rect);
 }
 
-fn draw_processesinfo<B: Backend>(f: &mut Frame<B>, rect: Rect, pl: &ProcessList) {
+fn draw_processesinfo<B: Backend>(f: &mut Frame<B>, rect: Rect, block: Block, pl: &ProcessList) {
     let selected_style = Style::default().add_modifier(Modifier::REVERSED);
-    let header_style = Style::default().bg(Color::DarkGray).fg(Color::Black);
+    let header_style = Style::default().bg(Color::DarkGray).fg(Color::White);
     let header_cells = [
         "PID", "PPID", "TID", "User", "Umask", "Threads", "Name", "State", "VM", "SM", "CMD",
     ]
-        .iter()
-        .map(|h| Cell::from(*h));
-    let header = Row::new(header_cells)
-        .style(header_style)
-        .height(1);
+    .iter()
+    .map(|h| Cell::from(*h));
+    let header = Row::new(header_cells).style(header_style).height(1);
     let rows = pl.processes.iter().map(|p| {
         let mut cells = Vec::new();
         cells.push(Cell::from(p.pid.to_string()));
@@ -370,18 +481,8 @@ fn draw_processesinfo<B: Backend>(f: &mut Frame<B>, rect: Rect, pl: &ProcessList
             Constraint::Length(9),
             Constraint::Min(1),
         ])
-        .block(
-            Block::default()
-            .title(Span::styled(
-                "Processes",
-                Style::default()
-                    .fg(Color::Cyan)
-                    .add_modifier(Modifier::BOLD),
-            ))
-            .borders(Borders::ALL),
-        );
+        .block(block);
     f.render_widget(table, rect);
-
 }
 
 fn size_columns(area_width: u16) -> Vec<Constraint> {
@@ -428,6 +529,7 @@ fn size_columns(area_width: u16) -> Vec<Constraint> {
 fn draw_networkinfo<B: Backend>(
     f: &mut Frame<B>,
     rect: Rect,
+    block: Block,
     last_info: &NetworkInfo,
     current_info: &NetworkInfo,
 ) {
@@ -435,14 +537,6 @@ fn draw_networkinfo<B: Backend>(
     let sending = to_humanreadable((current_info.send_bytes - last_info.send_bytes) * 10) + "/s";
     let total_received = to_humanreadable(current_info.rec_bytes);
     let total_sent = to_humanreadable(current_info.send_bytes);
-
-    let block = Block::default()
-        .title(Span::styled(
-        "Network",
-        Style::default()
-            .fg(Color::Cyan)
-            .add_modifier(Modifier::BOLD),
-        )).borders(Borders::ALL);
 
     let text = vec![
         Spans::from(format!("Receiving      {}", receiving)),
@@ -453,3 +547,5 @@ fn draw_networkinfo<B: Backend>(
     let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true });
     f.render_widget(paragraph, rect);
 }
+
+fn show_help_menu() {}
