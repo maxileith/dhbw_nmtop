f4f7210ddc56487f73526f6fdd99b8516d5b6907
Tobias <derrumpli+github@posteo.de>
2021-05-06 17:51:02 +0200

    fixes key lag

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
 use std::io;
 use std::sync::mpsc;
-use std::thread;
+use std::{thread, time::Instant, time::Duration};
 use termion::event::Key;
 use termion::input::TermRead;
 
@@ -14,10 +14,18 @@ impl InputHandler {
 
         thread::spawn(move || {
             let stdin = io::stdin();
+            let mut previous_key = Key::Null; 
+            let mut counter = 0;
+            let mut previous_time = Instant::now();
 
             for evt in stdin.keys() {
-                if let Ok(key) = evt {
-                    let _ = tx.send(key);
+                let m = previous_time.elapsed().as_millis();
+                
+                if m > 120 {
+                    if let Ok(key) = evt {
+                        let _ = tx.send(key);
+                    }
+                    previous_time = Instant::now();
                 }
             }
         });
